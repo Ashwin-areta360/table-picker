e82c58ea3891146c1340f6a57a1dcc3019e34972
Ubuntu <ubuntu@ip-172-31-29-106.us-east-2.compute.internal>
2026-04-07 09:06:05 +0000

    updated requiremrents,metadata and api.py

--- api.py
+++ api.py
@@ -149,4 +149,4 @@ def handle_query(payload: QueryRequest):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("api:app", host="127.0.0.1", port=9011, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=9019, reload=True)
